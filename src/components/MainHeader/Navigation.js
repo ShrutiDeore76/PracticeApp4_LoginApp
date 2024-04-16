1d04ab930513ade6418e0511508be181e8077698
ShrutiDeore76 <shrutideore76@gmail.com>
2024-04-16 18:06:09 +0530

    Adding dynamic context in case for logout

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../Context/auth-context';
 
-const Navigation = (props) => {
+const Navigation = () => {
+
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
           <nav className={classes.nav}>
             <ul>
               {ctx.isLoggedIn && (
@@ -22,15 +21,12 @@ const Navigation = (props) => {
               )}
               {ctx.isLoggedIn && (
                 <li>
-                  <button onClick={props.onLogout}>Logout</button>
+                  <button onClick={ctx.onLogout}>Logout</button>
                 </li>
               )}
             </ul>
           </nav>
-        );
-      }}
-    </AuthContext.Consumer>
-  );
+      );
 };
 
 export default Navigation;
